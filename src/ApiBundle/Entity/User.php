df4d275dfbecc5e651ec8e0dd77fe023e821c574
Thomas Caudrelier <tazukay@hotmail.fr>
2018-05-15 22:51:16 +0200

    Update config, delete unused bundle, add user entity, prepare ORM migrations

---
+++ src/ApiBundle/Entity/User.php
@@ -0,0 +1,22 @@
+namespace ApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $name;
+}
