62f68e09448085522c8bae9a4301e959dc922c8f
Thomas Caudrelier <tazukay@hotmail.fr>
2018-06-06 22:04:53 +0200

    Permet la modification partielle d'un produit

--- src/ApiBundle/Controller/ProductController.php
+++ src/ApiBundle/Controller/ProductController.php
@@ -6,6 +6,8 @@ use Swagger\Annotations as SWG;
 use ApiBundle\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
+use ApiBundle\Exception\BadRequestException;
 
 class ProductController extends AbstractController
 {
@@ -100,6 +102,52 @@ class ProductController extends AbstractController
         $em->flush();
 
         return self::createResponse($data);
+    }
+
+    /**
+     * @Rest\Put(
+     *      path = "/products/{id}",
+     * )
+     * @SWG\Response(
+     *      response = 200,
+     *      description="Returned when updated"
+     * )
+     * @SWG\Response(
+     *      response = 400,
+     *      description="Returned when a violation is raised by validation"
+     * )
+     */
+    public function updateAction(Request $req, Product $product)
+    {
+        if(null !== $req->get('name')) {
+            $product->setName($req->get('name'));
+        }
+
+        if(null !== $req->get('description')) {
+            $product->setDescription($req->get('description'));
+        }
+
+        if(null !== $req->get('price')) {
+            if(!is_float($req->get('price')) && !is_int($req->get('price'))) {
+                throw new BadRequestException("price should be a float value");
+            }
+            $product->setPrice($req->get('price'));
+        }
+
+        if(null !== $req->get('discount')) {
+            if(!is_float($req->get('discount')) && !is_int($req->get('discount'))) {
+                throw new BadRequestException("discount should be a float value");
+            }
+            if($req->get('discount') < 0 || $req->get('discount') > 1) {
+                throw new BadRequestException("discount should be between 0 and 1");
+            }
+            $product->setDiscount($req->get('discount'));
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+
+        return self::createResponse($product);
     }
 
     /**
