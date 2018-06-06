c201ed83aecb577295a19ee863a028a7488b1f63
Thomas Caudrelier <tazukay@hotmail.fr>
2018-06-06 22:39:55 +0200

    Add POST on /comments

--- src/ApiBundle/Controller/CommentController.php
+++ src/ApiBundle/Controller/CommentController.php
@@ -6,6 +6,8 @@ use Swagger\Annotations as SWG;
 use ApiBundle\Entity\Comment;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
+use ApiBundle\Exception\BadRequestException;
 
 class CommentController extends AbstractController
 {
@@ -54,8 +56,31 @@ class CommentController extends AbstractController
      *      description="Returned when a violation is raised by validation"
      * )
      */
-    public function createAction(Comment $data)
+    public function createAction(Request $req, Comment $data)
     {
+        $writer_id = $req->get('writer_id');
+        $product_id = $req->get('product_id');
+
+        if(null == $writer_id || !is_int($writer_id)) {
+            throw new BadRequestException("writer_id (integer) is needed");
+        }
+        if(null == $product_id || !is_int($product_id)) {
+            throw new BadRequestException("product_id (integer) is needed");
+        }
+
+        $command = $this->getDoctrine()->getRepository('ApiBundle:User')->find($writer_id);
+        if(null == $command) {
+            throw new BadRequestException("user (writer) with id " . $writer_id . " doesn't exist");
+        }
+
+        $product = $this->getDoctrine()->getRepository('ApiBundle:Product')->find($product_id);
+        if(null == $product) {
+            throw new BadRequestException("product_id " . $product_id . " doesn't exist");
+        }
+
+        $data->setWriter($command);
+        $data->setProduct($product);
+
         $em = $this->getDoctrine()->getManager();
 
         $em->merge($data);
