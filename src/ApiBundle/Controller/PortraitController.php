4ff780cf54a00f0a1550e43a38ba577586a0fbd4
Thomas Caudrelier <tazukay@hotmail.fr>
2018-06-07 22:38:38 +0200

    Remove unused code

--- src/ApiBundle/Controller/PortraitController.php
+++ src/ApiBundle/Controller/PortraitController.php
@@ -88,40 +88,4 @@ class PortraitController extends AbstractController
 
         return self::createResponse($portrait);
     }
-
-    /**
-     * @Rest\Delete(
-     *      path = "/access",
-     * )
-     * @SWG\Response(
-     *      response = 200,
-     *      description="Returned when deleted"
-     * )
-     * @SWG\Response(
-     *      response = 400,
-     *      description="Returned when a violation is raised by validation"
-     * )
-     */
-    public function deleteAction(Request $req)
-    {
-        $body = (array)json_decode($req->getContent(), true);
-
-        $user_id = $body['user_id'];
-        $connected_object_id = $body['connected_object_id'];
-
-        if(!isset($user_id) || !is_int($user_id)) {
-            throw $this->createNotFoundException("'user_id' (integer) must appear in body");
-        }
-
-        if(!isset($connected_object_id) || !is_int($connected_object_id)) {
-            throw $this->createNotFoundException("'connected_object_id' (integer) must appear in body");
-        }
-
-        $em = $this->getDoctrine()->getManager();
-        $access = $em->find("ApiBundle\Entity\Access", array("user" => $user_id, "connectedObject" => $connected_object_id));
-        $em->remove($access);
-        $em->flush();
-
-        return self::createResponse($access);
-    }
 }
