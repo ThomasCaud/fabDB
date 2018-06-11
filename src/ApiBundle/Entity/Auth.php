3ca1f9692519334be9246fcda9f83bd35093ac7a
Maxime LUCAS <maxime.lucas96@gmail.com>
2018-06-11 19:16:50 +0200

    Here we are bitches - Beginning of Authentication feature

---
+++ src/ApiBundle/Entity/Auth.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace ApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Auth
+ *
+ * @ORM\Table(name="auth")
+ * @ORM\Entity(repositoryClass="ApiBundle\Repository\AuthRepository")
+ */
+class Auth
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="login", type="string", length=255, unique=true)
+     */
+    private $login;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="plainPassword", type="string", length=255)
+     */
+    private $plainPassword;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set login.
+     *
+     * @param string $login
+     *
+     * @return Auth
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * Get login.
+     *
+     * @return string
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set password.
+     *
+     * @param string $password
+     *
+     * @return Auth
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password.
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set plainPassword.
+     *
+     * @param string $plainPassword
+     *
+     * @return Auth
+     */
+    public function setPlainPassword($plainPassword)
+    {
+        $this->plainPassword = $plainPassword;
+
+        return $this;
+    }
+
+    /**
+     * Get plainPassword.
+     *
+     * @return string
+     */
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+}
