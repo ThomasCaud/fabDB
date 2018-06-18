5fe3a3a8fb022028fff77e09a252d40e34b80f78
Maxime LUCAS <maxime.lucas@equensworldline.com>
2018-06-18 16:24:58 +0200

    Adding another migration

--- app/DoctrineMigrations/Version20180618132207.php
+++ app/DoctrineMigrations/Version20180618132207.php
@@ -13,9 +13,17 @@ final class Version20180618132207 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         $this->addSql("
+            ALTER TABLE comment AUTO_INCREMENT = 1;
             INSERT INTO comment (writer_id,product_id,note,comment,date) VALUES
             (1,1,5,'Produit arrivé sans emballage. Je suis déçu.','2018-06-18'),
-            (1,2,15,'Mon chien s\'éclate déjà avec son nouveau chat en plastique à dévorer, merci au maker !','2018-06-18')
+            (1,2,15,'Mon chien s\'éclate déjà avec son nouveau chat en plastique à dévorer, merci au maker !','2018-06-18');
+
+            ALTER TABLE family_member AUTO_INCREMENT = 1;
+            INSERT INTO family_member (referrer_id,user_account_id,birthday,relationship,sexe) VALUES
+            (1,2,'1996-08-18','son','O');
+
+            INSERT INTO family_member (referrer_id,user_account_id,birthday,relationship,sexe) VALUES
+            (2,1,'1996-04-19','father','O');
         ");
     }
 
@@ -23,6 +31,7 @@ final class Version20180618132207 extends AbstractMigration
     {
         $this->addSql("
             DELETE FROM comment WHERE id < 3;
+            DELETE FROM family_member WHERE id < 3;
         ");
     }
 }
